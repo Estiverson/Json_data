9ba3aef5859aaabe6cc32ea023dee7eca3436d63
Estiverson <40440392+Estiverson@users.noreply.github.com>
2018-07-14 15:42:35 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String url = 'http://192.168.0.75/werkzeug/public/api/data_tool1';
+  List data;
+  Future<String> makeRequest() async {
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    setState(() {
+      var extractdata = JSON.decode(response.body);
+      data = extractdata;
+    });
+  }
+
+  @override
+  void initState() {
+    this.makeRequest();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Contact List'),
+        ),
+        body: new ListView.builder(
+            itemCount: data == null ? 0 : data.length,
+            itemBuilder: (BuildContext context, int i) {
+              return new ListTile(
+                title: new Text('Werkzeug 1 Temperatur beträgt: ''${data[i]["temp1"]}'
+                    'Und bei Werkzeug 2 sind es:  ''${data[i]["temp2"]}'),
+              );
+            }
+        )
+    );
+  }
+}
